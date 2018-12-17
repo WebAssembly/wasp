f8db2f930ce0bfba6d6964f2d4a9df7d4541e90d
Ben Smith <binjimin@gmail.com>
2018-12-16 18:41:15 -0800

    Works pretty well!

--- src/view/view_main.cc
+++ src/view/view_main.cc
@@ -37,6 +37,9 @@ using namespace ::wasp::binary;
 static std::string s_filename;
 static std::vector<u8> s_buffer;
 
+// TODO This should be a named value, external kind count.
+static u32 s_import_count[4];
+
 template <typename T>
 void DumpSection(T section, string_view name) {
   if (section.count) {
@@ -49,12 +52,64 @@ void DumpSection(T section, string_view name) {
 }
 
 template <>
-void DumpSection<LazyCodeSection<ErrorsVector>>(
-    LazyCodeSection<ErrorsVector> section,
+void DumpSection<LazyFunctionSection<ErrorsNop>>(
+    LazyFunctionSection<ErrorsNop> section,
     string_view name) {
   if (section.count) {
     ImGui::Text("%s", format("  {}[{}]\n", name, *section.count).c_str());
-    Index count = 0;
+    Index count = s_import_count[static_cast<size_t>(ExternalKind::Function)];
+    for (auto item : section.sequence) {
+      ImGui::Text("%s", format("    [{}]: {}\n", count++, item).c_str());
+    }
+  }
+}
+
+template <>
+void DumpSection<LazyTableSection<ErrorsNop>>(
+    LazyTableSection<ErrorsNop> section,
+    string_view name) {
+  if (section.count) {
+    ImGui::Text("%s", format("  {}[{}]\n", name, *section.count).c_str());
+    Index count = s_import_count[static_cast<size_t>(ExternalKind::Table)];
+    for (auto item : section.sequence) {
+      ImGui::Text("%s", format("    [{}]: {}\n", count++, item).c_str());
+    }
+  }
+}
+
+template <>
+void DumpSection<LazyMemorySection<ErrorsNop>>(
+    LazyMemorySection<ErrorsNop> section,
+    string_view name) {
+  if (section.count) {
+    ImGui::Text("%s", format("  {}[{}]\n", name, *section.count).c_str());
+    Index count = s_import_count[static_cast<size_t>(ExternalKind::Memory)];
+    for (auto item : section.sequence) {
+      ImGui::Text("%s", format("    [{}]: {}\n", count++, item).c_str());
+    }
+  }
+}
+
+template <>
+void DumpSection<LazyGlobalSection<ErrorsNop>>(
+    LazyGlobalSection<ErrorsNop> section,
+    string_view name) {
+  if (section.count) {
+    ImGui::Text("%s", format("  {}[{}]\n", name, *section.count).c_str());
+    Index count = s_import_count[static_cast<size_t>(ExternalKind::Global)];
+    for (auto item : section.sequence) {
+      ImGui::Text("%s", format("    [{}]: {}\n", count++, item).c_str());
+    }
+  }
+}
+
+
+template <>
+void DumpSection<LazyCodeSection<ErrorsNop>>(LazyCodeSection<ErrorsNop> section,
+                                             string_view name) {
+  if (section.count) {
+    ImGui::Text("%s", format("  {}[{}]\n", name, *section.count).c_str());
+    Index count = s_import_count[static_cast<size_t>(ExternalKind::Function)];
     for (auto item : section.sequence) {
       if (ImGui::TreeNode(
               reinterpret_cast<void*>(count), "%s",
@@ -98,84 +153,95 @@ void ViewInit(int argc, char** argv) {
   }
 
   s_buffer = std::move(*optbuf);
+  SpanU8 data{s_buffer};
+
+  ErrorsNop errors;
+  auto module = ReadModule(data, errors);
+  for (auto section : module.sections) {
+    if (section.is_known()) {
+      auto known = section.known();
+      if (known.id == SectionId::Import) {
+        auto sec = ReadImportSection(known, errors);
+        for (auto import : sec.sequence) {
+          s_import_count[static_cast<size_t>(import.kind())]++;
+        }
+      }
+    }
+  }
+}
+
+void ViewSection(KnownSection<> known, ErrorsNop& errors) {
+  if (ImGui::Begin(format("{}", known.id).c_str())) {
+    switch (known.id) {
+      case SectionId::Custom:
+        WASP_UNREACHABLE();
+        break;
+
+      case SectionId::Type:
+        DumpSection(ReadTypeSection(known, errors), "Type");
+        break;
+
+      case SectionId::Import:
+        DumpSection(ReadImportSection(known, errors), "Import");
+        break;
+
+      case SectionId::Function:
+        DumpSection(ReadFunctionSection(known, errors), "Func");
+        break;
+
+      case SectionId::Table:
+        DumpSection(ReadTableSection(known, errors), "Table");
+        break;
+
+      case SectionId::Memory:
+        DumpSection(ReadMemorySection(known, errors), "Memory");
+        break;
+
+      case SectionId::Global:
+        DumpSection(ReadGlobalSection(known, errors), "Global");
+        break;
+
+      case SectionId::Export:
+        DumpSection(ReadExportSection(known, errors), "Export");
+        break;
+
+      case SectionId::Start: {
+        auto section = ReadStartSection(known, errors);
+        size_t count = section.start() ? 1 : 0;
+        print("  Start[{}]\n", count);
+        if (count > 0) {
+          print("    [0]: {}\n", *section.start());
+        }
+        break;
+      }
+
+      case SectionId::Element:
+        DumpSection(ReadElementSection(known, errors), "Element");
+        break;
+
+      case SectionId::Code:
+        DumpSection(ReadCodeSection(known, errors), "Code");
+        break;
+
+      case SectionId::Data:
+        DumpSection(ReadDataSection(known, errors), "Data");
+        break;
+    }
+  }
+  ImGui::End();
 }
 
 void ViewMain() {
   SpanU8 data{s_buffer};
-  ErrorsVector errors;
+  ErrorsNop errors;
   auto module = ReadModule(data, errors);
 
-  if (ImGui::Begin("View")) {
-    intptr_t i = 0;
-    for (auto section : module.sections) {
-      if (section.is_known()) {
-        auto known = section.known();
-        if (ImGui::TreeNode(
-                reinterpret_cast<void*>(i), "%s",
-                format("section {}: {} bytes", known.id, known.data.size())
-                    .c_str())) {
-          switch (known.id) {
-            case SectionId::Custom:
-              WASP_UNREACHABLE();
-              break;
-
-            case SectionId::Type:
-              DumpSection(ReadTypeSection(known, errors), "Type");
-              break;
-
-            case SectionId::Import:
-              DumpSection(ReadImportSection(known, errors), "Import");
-              break;
-
-            case SectionId::Function:
-              DumpSection(ReadFunctionSection(known, errors), "Func");
-              break;
-
-            case SectionId::Table:
-              DumpSection(ReadTableSection(known, errors), "Table");
-              break;
-
-            case SectionId::Memory:
-              DumpSection(ReadMemorySection(known, errors), "Memory");
-              break;
-
-            case SectionId::Global:
-              DumpSection(ReadGlobalSection(known, errors), "Global");
-              break;
-
-            case SectionId::Export:
-              DumpSection(ReadExportSection(known, errors), "Export");
-              break;
-
-            case SectionId::Start: {
-              auto section = ReadStartSection(known, errors);
-              size_t count = section.start() ? 1 : 0;
-              print("  Start[{}]\n", count);
-              if (count > 0) {
-                print("    [0]: {}\n", *section.start());
-              }
-              break;
-            }
-
-            case SectionId::Element:
-              DumpSection(ReadElementSection(known, errors), "Element");
-              break;
-
-            case SectionId::Code:
-              DumpSection(ReadCodeSection(known, errors), "Code");
-              break;
-
-            case SectionId::Data:
-              DumpSection(ReadDataSection(known, errors), "Data");
-              break;
-          }
-          ImGui::TreePop();
-        }
-      }
-      ++i;
+  for (auto section : module.sections) {
+    if (section.is_known()) {
+      auto known = section.known();
+      ViewSection(known, errors);
     }
   }
-  ImGui::End();
 }
 
 }  // namespace view
