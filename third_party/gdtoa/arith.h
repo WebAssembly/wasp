61e52ea0b84d51dc574b3d5cbd47967ced5179de
Ben Smith <binji@chromium.org>
2020-11-07 20:15:16 -0800

    Remove third_party/gdtoa; use absl (#24)

--- third_party/gdtoa/arith.h
+++
@@ -1,8 +0,0 @@
-// Created by running arithchk in third_party/gdtoa
-// TODO: use CMake defines to configure as necessary
-#define IEEE_8087
-#define Arith_Kind_ASL 1
-#define Long int
-#define Intcast (int)(long)
-#define Double_Align
-#define X64_bit_pointers
