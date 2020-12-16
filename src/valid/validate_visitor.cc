c4ed2dee5f0d6d6abf7c7bb975cd557919ee103b
Ben Smith <binjimin@gmail.com>
2020-12-15 16:20:27 -0800

    Validate Events in ValidateVisitor

--- src/valid/validate_visitor.cc
+++ src/valid/validate_visitor.cc
@@ -57,6 +57,10 @@ auto ValidateVisitor::OnGlobal(const At<binary::Global>& global) -> Result {
   return FailUnless(Validate(ctx, global));
 }
 
+auto ValidateVisitor::OnEvent(const At<binary::Event>& event) -> Result {
+  return FailUnless(Validate(ctx, event));
+}
+
 auto ValidateVisitor::OnExport(const At<binary::Export>& export_) -> Result {
   return FailUnless(Validate(ctx, export_));
 }
